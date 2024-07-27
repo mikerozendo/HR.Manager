653add66d78100d4695d7927d57a7e740fee656c
mikerozendo <mikerozendo@gmail.com>
2024-07-27 10:07:34 -0300

    creating project structure

---
+++ Sales.Backoffice/src/Sales.Backoffice.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sales.Backoffice.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
