1c89b7ec285970b92a5e59e1c83090e72c71f9cd
mikerozendo <mikerozendo@gmail.com>
2024-08-03 01:22:03 -0300

    creating individualperson base form

--- Sales.Backoffice/src/Sales.Backoffice.Dto/Requests/CreateEmployeeRequest.cs
+++ Sales.Backoffice/src/Sales.Backoffice.Dto/Requests/CreateEmployeeRequest.cs
@@ -8,5 +8,8 @@ public class CreateEmployeeRequest : IRequest<Result>
     public string Name { get; set; }
     public string LastName { get; set; }
     public string Rg { get; set; }
-    public string Cpf { get; set; } 
+    public string Cpf { get; set; }
+    public DateOnly BirthDate { get; set; }
+    public int SexType { get; set; }
+    public string ZipCode { get; set; }
 }
