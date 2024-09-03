0a9f96dab4f19cf167500ca64712a43d81819089
mikerozendo <mikerozendo@gmail.com>
2024-09-03 02:15:47 -0300

    refactor + dockerfile model

--- Sales.Backoffice/src/Services/WebApi/Sales.Backoffice.WebApi/Program.cs
+++ Sales.Backoffice/src/Services/WebApi/Sales.Backoffice.WebApi/Program.cs
@@ -1,76 +1,16 @@
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
-using Sales.Backoffice.Application.Handlers.Commands;
-using Sales.Backoffice.Repository;
-using Sales.Backoffice.Repository.Internal;
-using Sales.Backoffice.Repository.Internal.Interfaces;
-using Sales.Backoffice.Repository.Seeders;
-using Sales.Backoffice.WebApi.Configuration;
+using Sales.Backoffice.WebApi;
 
-
-var builder = WebApplication.CreateBuilder(args);
-
-
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<CreateEmployeeRequestHandler>());
-
-builder.Services.AddScoped<IDepartmentRepository, DepartmentRepository>();
-builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
-builder.Services.AddScoped<IAgentRepository, AgentRepository>();
-builder.Services.AddScoped<IDocumentRepository, DocumentRepository>();
-
-var envConfig = builder.Configuration.Get<EnvironmentConfiguration>();
-
-
-builder.Services.AddDbContextPool<ApplicationDbContext>(
-	opt => opt.UseSqlServer(envConfig.ConnectionStrings.SqlServer, 
-		migrations => migrations.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName))
-	.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking)
-	.UseLazyLoadingProxies(false)
-	.UseChangeTrackingProxies(false, false)
-	.EnableThreadSafetyChecks(false)
-	//UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery) //I need to read more about this 
-);
-
-DepartmentSeeder.Seed(builder.Services.BuildServiceProvider());
-
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-	.AddJwtBearer(opt =>
-	{
-		opt.Authority = envConfig.IdentityConfig.Url; 
-		opt.Audience = "sales_backoffice_webapi";
-		opt.TokenValidationParameters.ValidTypes = new[] { "at+jwt" };
-	});
-
-builder.Services.AddAuthorizationBuilder()
-	.AddPolicy("ApiScope", policy =>
-	{
-		policy.RequireAuthenticatedUser();
-		policy.RequireClaim(
-			"scope",
-			envConfig.IdentityConfig.Scope);
-	});
-
-var app = builder.Build();
-if (app.Environment.IsDevelopment())
+public class Program
 {
-	app.UseSwagger();
-	app.UseSwaggerUI(c =>
-	{
-		c.SwaggerEndpoint("/swagger/v1/swagger.json", "Sales.Backoffice.WebApi");
-	});
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
 }
-
-// app.UseHttpsRedirection();
-app.UseRouting();
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.UseEndpoints(endpoints =>
-{
-	endpoints.MapControllers().RequireAuthorization("ApiScope");
-});
-
-app.Run();
---
+++ Sales.Backoffice/src/Services/WebApi/Sales.Backoffice.WebApi/Startup.cs
@@ -0,0 +1,93 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Sales.Backoffice.Application.Handlers.Commands;
+using Sales.Backoffice.Repository;
+using Sales.Backoffice.Repository.Internal;
+using Sales.Backoffice.Repository.Internal.Interfaces;
+using Sales.Backoffice.Repository.Seeders;
+using Sales.Backoffice.WebApi.Configuration;
+
+namespace Sales.Backoffice.WebApi;
+
+
+public class Startup
+{
+	public Startup(IConfiguration configuration)
+	{
+		Configuration = configuration;
+	}
+
+	public IConfiguration Configuration { get; }
+
+	public void Configureservices(IServiceCollection services)
+	{
+		// Adicione os serviços necessários para a aplicação.
+		services.AddControllers(); // Exemplo de serviço
+		
+		
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<CreateEmployeeRequestHandler>());
+
+        services.AddScoped<IDepartmentRepository, DepartmentRepository>();
+        services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+        services.AddScoped<IAgentRepository, AgentRepository>();
+        services.AddScoped<IDocumentRepository, DocumentRepository>();
+
+        var envConfig = Configuration.Get<EnvironmentConfiguration>();
+
+
+        services.AddDbContextPool<ApplicationDbContext>(
+            opt => opt.UseSqlServer(envConfig.ConnectionStrings.SqlServer,
+                migrations => migrations.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName))
+            .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking)
+            .UseLazyLoadingProxies(false)
+            .UseChangeTrackingProxies(false, false)
+            .EnableThreadSafetyChecks(false)
+        //UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery) //I need to read more about this 
+        );
+
+        services.BuildServiceProvider().Seed();
+
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(opt =>
+            {
+                opt.Authority = envConfig.IdentityConfig.Url;
+                opt.Audience = "sales_backoffice_webapi";
+                opt.TokenValidationParameters.ValidTypes = new[] { "at+jwt" };
+            });
+
+        services.AddAuthorizationBuilder()
+            .AddPolicy("ApiScope", policy =>
+            {
+                policy.RequireAuthenticatedUser();
+                policy.RequireClaim(
+                    "scope",
+                    envConfig.IdentityConfig.Scope);
+            });
+
+	}
+
+	public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+	{
+	  if (env.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Sales.Backoffice.WebApi");
+            });
+        }
+
+        // app.UseHttpsRedirection();
+        app.UseRouting();
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers().RequireAuthorization("ApiScope");
+        });
+	}
+}
