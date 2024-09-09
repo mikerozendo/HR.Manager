c991d7313b72513d5bdd1b380b033ef3b2277a1f
mikerozendo <mikerozendo@gmail.com>
2024-09-09 18:37:27 -0300

    adjusting envvars

--- Sales.Backoffice/src/Presentation/Sales.Backoffice.Web/Controllers/HomeController.cs
+++ Sales.Backoffice/src/Presentation/Sales.Backoffice.Web/Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using System.Diagnostics;
 
 namespace Sales.Backoffice.Web.Controllers;
 
-[Authorize]
+// [Authorize]
 public class HomeController() : Controller
 {
     public IActionResult Index()
--- Sales.Backoffice/src/Presentation/Sales.Backoffice.Web/Program.cs
+++ Sales.Backoffice/src/Presentation/Sales.Backoffice.Web/Program.cs
@@ -10,76 +10,64 @@ internal class Program
 {
    private static void Main(string[] args)
    {
-       var builder = WebApplication.CreateBuilder(args);
+	   var builder = WebApplication.CreateBuilder(args);
 
 
-       var configuration = builder.Configuration.Get<EnvironmentConfiguration>();
+	   var configuration = builder.Configuration.Get<EnvironmentConfiguration>();
 
-       builder.Services.AddHttpContextAccessor();
-       builder.Services.AddControllersWithViews();
-       builder.Services.AddTransient<AuthenticationHandler>();
-        
-       builder.Services
-        .AddRefitClient<IWebApiClient>()
-        .AddHttpMessageHandler<AuthenticationHandler>()
-        .ConfigureHttpClient(c => c.BaseAddress = new Uri(configuration.WebServiceUrls.SalesBackofficeWebApi));
+	   builder.Services.AddHttpContextAccessor();
+	   builder.Services.AddControllersWithViews();
+	   builder.Services.AddTransient<AuthenticationHandler>();
+		
+	   builder.Services
+		.AddRefitClient<IWebApiClient>()
+		.AddHttpMessageHandler<AuthenticationHandler>()
+		.ConfigureHttpClient(c => c.BaseAddress = new Uri(configuration.WebServiceUrls.SalesBackofficeWebApi));
 
-    //    var httpsConnectionAdapterOptions = new HttpsConnectionAdapterOptions
-    //    {
-    //        SslProtocols = System.Security.Authentication.SslProtocols.Tls12,
-    //        ClientCertificateMode = ClientCertificateMode.AllowCertificate,
-    //        ServerCertificate = new X509Certificate2($"{Directory.GetCurrentDirectory()}\\certificate.pfx", "password")
-    //    };
+	   builder.Services.AddAuthentication(options =>
+	   {
+		   options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+		   options.DefaultChallengeScheme = "oidc";
+	   })
+	   .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme,
+		   x =>
+		   {
+			   x.ExpireTimeSpan = TimeSpan.FromMinutes(10);
+		   }
+	   )
+	   .AddOpenIdConnect("oidc", opt =>
+	   {
+		   opt.Authority = configuration.WebServiceUrls.SalesBackofficeIdentity;
+		   opt.GetClaimsFromUserInfoEndpoint = true;
+		   opt.ClientId = configuration.Identity.ClientId;
+		   opt.ClientSecret = configuration.Identity.ClientSecret;
+		   opt.ResponseType = configuration.Identity.ResponseType;
+		   opt.Scope.Add(configuration.Identity.AllowedScopes);
+		   opt.SaveTokens = true;
+		   opt.UseTokenLifetime = true;
+		   opt.RequireHttpsMetadata = false;//only for dev env
+	   });
 
+	   var app = builder.Build();
 
-    //    builder.WebHost.ConfigureKestrel(options =>
-    //    options.ConfigureEndpointDefaults(listenOptions =>
-    //    listenOptions.UseHttps(httpsConnectionAdapterOptions)));
+	   if (!app.Environment.IsDevelopment())
+	   {
+		   app.UseExceptionHandler("/Home/Error");
+		   app.UseHsts();
+	   }
 
+	//    app.UseHttpsRedirection();
+	   app.UseMiddleware<ExceptionMiddleware>();
 
-       builder.Services.AddAuthentication(options =>
-       {
-           options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-           options.DefaultChallengeScheme = "oidc";
-       })
-       .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme,
-           x =>
-           {
-               x.ExpireTimeSpan = TimeSpan.FromMinutes(10);
-           }
-       )
-       .AddOpenIdConnect("oidc", opt =>
-       {
-           opt.Authority = configuration.WebServiceUrls.SalesBackofficeIdentity;
-           opt.GetClaimsFromUserInfoEndpoint = true;
-           opt.ClientId = configuration.Identity.ClientId;
-           opt.ClientSecret = configuration.Identity.ClientSecret;
-           opt.ResponseType = configuration.Identity.ResponseType;
-           opt.Scope.Add(configuration.Identity.AllowedScopes);
-           opt.SaveTokens = true;
-           opt.UseTokenLifetime = true;
-       });
+	   app.UseStaticFiles();
 
-       var app = builder.Build();
+	//    app.UseRouting();
+	   app.UseAuthentication();
+	   app.UseAuthorization();
+	   app.MapControllerRoute(
+		   name: "default",
+		   pattern: "{controller=Home}/{action=Index}/{id?}");
 
-       if (!app.Environment.IsDevelopment())
-       {
-           app.UseExceptionHandler("/Home/Error");
-           app.UseHsts();
-       }
-
-       app.UseHttpsRedirection();
-       app.UseMiddleware<ExceptionMiddleware>();
-
-       app.UseStaticFiles();
-
-       app.UseRouting();
-       app.UseAuthentication();
-       app.UseAuthorization();
-       app.MapControllerRoute(
-           name: "default",
-           pattern: "{controller=Home}/{action=Index}/{id?}");
-
-       app.Run();
+	   app.Run();
    }
 }
