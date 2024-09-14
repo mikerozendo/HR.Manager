a3910141fe947f4049ee97fdd21faff69a8dbe32
mikerozendo <mikerozendo@gmail.com>
2024-09-14 14:59:46 -0300

    removing env validation to use swagger from webapi

--- Sales.Backoffice/src/Services/WebApi/Sales.Backoffice.WebApi/Program.cs
+++ Sales.Backoffice/src/Services/WebApi/Sales.Backoffice.WebApi/Program.cs
@@ -7,16 +7,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 var configuration = builder.Configuration;
 var envConfig = configuration.Get<EnvironmentConfiguration>()
-	?? throw new ArgumentNullException("Missing configuration file");
-	
+    ?? throw new ArgumentNullException("Missing configuration file");
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c => 
-	c.SwaggerDoc("v1", new OpenApiInfo { Title = "Sales.Backoffice.WebApi", Version = "v1" })
+builder.Services.AddSwaggerGen(c =>
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Sales.Backoffice.WebApi", Version = "v1" })
 );
-	
-builder.Services.AddMediatR(cfg => 
-	cfg.RegisterServicesFromAssemblyContaining<CreateEmployeeRequestHandler>()
+
+builder.Services.AddMediatR(cfg =>
+    cfg.RegisterServicesFromAssemblyContaining<CreateEmployeeRequestHandler>()
 );
 
 builder.Services
@@ -25,14 +25,12 @@ builder.Services
 .AddIdentityConfig(envConfig);
 
 if (envConfig.ShouldDbBePopulated)
-	await builder.Services.SeedDatabaseAsync();
+    await builder.Services.SeedDatabaseAsync();
 
 var app = builder.Build();
-if (app.Environment.IsDevelopment())
-{
-	app.UseSwagger();
-	app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Sales.Backoffice.WebApi v1"));
-}
+app.UseSwagger();
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Sales.Backoffice.WebApi v1"));
+
 
 app.UseHttpsRedirection();
 app.UseRouting();
