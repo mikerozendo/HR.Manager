d8cd753a61ecc3f6a0c4245e170016a2227394e0
mikerozendo <mikerozendo@gmail.com>
2024-09-18 20:22:04 -0300

    adjusting CreateEmployee form

--- Sales.Backoffice/src/Presentation/Sales.Backoffice.Web/Repositories/Interfaces/IWebApiClient.cs
+++ Sales.Backoffice/src/Presentation/Sales.Backoffice.Web/Repositories/Interfaces/IWebApiClient.cs
@@ -1,4 +1,3 @@
-using Microsoft.AspNetCore.Mvc;
 using Refit;
 using Sales.Backoffice.Dto.Enums;
 using Sales.Backoffice.Dto.Requests.Commands;
@@ -6,21 +5,20 @@ using Sales.Backoffice.Dto.Responses;
 
 namespace Sales.Backoffice.Web.Repositories.Interfaces;
 
-
 public interface IWebApiClient
 {
 	[Post("/api/employees")]
 	Task PostAsync(CreateEmployeeRequest createEmployeeRequest);
-	
-	[Get("/employees/{id}")]
+
+    [Get("/employees/{id}")]
 	Task<ApiResponse<GetEmployeeResponse>> GetByIdAsync(Guid id);
-	
+
 	[Get("/api/employees/{filterOption}/get-by-filter")]
 	Task<ApiResponse<IEnumerable<GetEmployeeResponse>>> GetEmployeesByFilterAsync(ActiveEmployeeOptionsDto filterOption);
-	
+
 	[Get("/api/employees/{departmentType}/get-by-department")]
 	Task<ApiResponse<IEnumerable<GetEmployeeResponse>>> GetEmployeesByDepartmentAsync(DepartmentTypeDto departmentType);
-	
+
 	[Delete("/api/agents/{id}")]
 	Task DeleteAsync(Guid id);
 }
